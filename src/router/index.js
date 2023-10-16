9768277b20c9a6e8c7450b78c54299f6113fa51e
shaoairai <shaoairai@gmail.com>
2023-10-16 13:38:14 +0800

    修改：路由寫法調整，解決初次載入過慢問題

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/front/HomeView.vue";
 import LoginView from "../views/login/LoginView.vue";
+import AboutView from "../views/front/AboutView.vue";
+import ProductsView from "../views/front/ProductsView.vue";
+import ProductView from "../views/front/ProductView.vue";
+import CartView from "../views/front/CartView.vue";
+import ContactView from "../views/front/ContactView.vue";
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -18,27 +23,32 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
-      component: () => import("../views/front/AboutView.vue"),
+      component: AboutView,
+      // component: () => import("../views/front/AboutView.vue"),
     },
     {
       path: "/products",
       name: "products",
-      component: () => import("../views/front/ProductsView.vue"),
+      component: ProductsView,
+      // component: () => import("../views/front/ProductsView.vue"),
     },
     {
       path: "/product/:id",
       name: "product",
-      component: () => import("../views/front/ProductView.vue"),
+      component: ProductView,
+      // component: () => import("../views/front/ProductView.vue"),
     },
     {
       path: "/cart",
       name: "cart",
-      component: () => import("../views/front/CartView.vue"),
+      component: CartView,
+      // component: () => import("../views/front/CartView.vue"),
     },
     {
       path: "/contact",
       name: "contact",
-      component: () => import("../views/front/ContactView.vue"),
+      component: ContactView,
+      // component: () => import("../views/front/ContactView.vue"),
     },
   ],
 });
--- src/views/front/ProductView.vue
+++ src/views/front/ProductView.vue
@@ -1,5 +1,6 @@
 <script>
 import { RouterLink } from 'vue-router';
+
 export default {
   props: ['products'],
   data() {
